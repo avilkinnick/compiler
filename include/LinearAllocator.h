3ef107e9ed6bd77ae0a502f2225f7496749ae35b
avilkinnick <avilkin.nick@gmail.com>
2026-01-13 00:10:49 +0300

    Generic create arrays prototype

--- include/LinearAllocator.h
+++ include/LinearAllocator.h
@@ -11,11 +11,29 @@ typedef struct LinearAllocator
     size_t capacity;
 } LinearAllocator;
 
+typedef enum LinearAllocatorResult
+{
+    LINEAR_ALLOCATOR_SUCCESS,
+    LINEAR_ALLOCATOR_ERROR_OVERFLOW,
+    LINEAR_ALLOCATOR_ERROR_CREATE,
+    LINEAR_ALLOCATOR_ERROR_ALLOCATE
+} LinearAllocatorResult;
+
 void linear_allocator_zero(LinearAllocator* const allocator);
 
 bool linear_allocator_create(
     LinearAllocator* const allocator,
     const size_t capacity
+);
+
+LinearAllocatorResult linear_allocator_create_arrays(
+    LinearAllocator* const allocator,
+    const int array_count,
+    const size_t element_count,
+    size_t* const sizes,
+    const size_t* const alignments,
+    const size_t additional_memory_size,
+    size_t* const * const offsets
 );
 
 void linear_allocator_destroy(LinearAllocator* const allocator);
